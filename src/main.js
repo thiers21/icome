228e55a5854cb862246e997f0315e77b4a126f95
thiers21 <thierry.ndri85@gmail.com>
2020-11-21 18:27:09 +0000

    Initialisation et configuration du projet

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
 
+//configuration de parse server
+const Parse = require('parse');
+Parse.initialize("GAeDVnUM3Y3TBqVmggHKjNQeAIzObVNFw06aWJ7K", "uzx41wQlfzfoQouTZk06emGeGMWSi4CiOcQMw89k");
+Parse.serverURL = "https://parseapi.back4app.com/";
 new Vue({
   render: h => h(App),
 }).$mount('#app')
